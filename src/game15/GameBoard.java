985ab37e2ff0002ce0a537c5ace15e2a251bbc6a
Karwan Ali <karwana0000@gmail.com>
2025-10-30 14:51:44 +0100

    added array, setup constructor for GameBoard, for loop for tiles

--- src/game15/GameBoard.java
+++ src/game15/GameBoard.java
@@ -1,4 +1,27 @@
 package game15;
 
 public class GameBoard {
+    private Tile[][] board;
+    private int rowEmpty;
+    private int columnEmpty;
+
+    public GameBoard() {
+        board = new Tile[4][4];
+        setupBoard();
+    }
+    public void setupBoard() {
+        int number = 1;
+
+        for (int row = 0; row < 4; row++) {
+            for (int column = 0; column < 4; column++) {
+
+                if (row == 3 && column == 3) {
+                    board[row][column] = null;
+                    rowEmpty = 3;
+                    columnEmpty = 3;
+                }
+            }
+        }
+    }
+
 }
