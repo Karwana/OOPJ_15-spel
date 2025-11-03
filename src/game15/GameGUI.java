0b6ee58ec9d2f885bb545a560d1f125b8d50c6c7
Karwan Ali <karwana0000@gmail.com>
2025-11-03 12:52:03 +0100

    experimenting with size + layout

--- src/game15/GameGUI.java
+++ src/game15/GameGUI.java
@@ -1,19 +1,39 @@
 package game15;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GameGUI extends JFrame implements ActionListener {
 
+    private GameBoard game;
+    private JButton[][] buttons;
 
+    public GameGUI() {
+        game = new GameBoard();
+        game.shuffle();
+        buttons = new JButton[4][4];
+        setLayout(new GridLayout(4,4));
 
-
-
-
+        for (int row = 0; row < 4; row++) {
+            for (int column = 0; column < 4; column++) {
+                buttons[row][column] = new JButton();
+                buttons[row][column].addActionListener(this);
+                add(buttons[row][column]);
+            }
+        }
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    static void main() {
+        new GameGUI();
     }
 }
