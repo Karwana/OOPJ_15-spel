4af63da272bd6a7e9268e18f160a1395a5c25235
Fahim Khan <abrar.fahimk98@gmail.com>
2025-11-03 11:49:47 +0100

    name changes

--- src/game15/Tile.java
+++ src/game15/Tile.java
@@ -7,12 +7,12 @@ public class Tile {
     private int currentRow;        // Nuvarande rad (0-3)
     private int currentColumn;     // Nuvarande kolumn (0-3)
 
-    public Tile(int number, int currentRow, int col) {
+    public Tile(int number, int row, int column) {
         this.number = number;
-        this.currentRow = currentRow;
-        this.currentColumn = col;
+        this.currentRow = row;
+        this.currentColumn = column;
     }
-    // Getters
+
     public int getNumber() {
         return number;
     }
@@ -25,18 +25,18 @@ public class Tile {
         return currentColumn;
     }
     // Uppdatera brickans position när den flyttas
-    public void setPosition(int row, int col) {
+    public void setPosition(int row, int column) {
         this.currentRow = row;
-        this.currentColumn = col;
+        this.currentColumn = column;
     }
     // Kolla om brickan är på rätt plats
     // Bricka 1 ska vara på (0,0) , bricka 2 på (0,1) , bricka 5 på (1,0)
     public boolean isInCorrectPosition() {
         int correctRow = (number - 1) / 4;
-        int correctCol = (number - 1) % 4;
+        int correctColumn = (number - 1) % 4;
 
         // Kolla om nuvarande position matchar rätt position
-        if (currentRow == correctRow && currentColumn == correctCol) {
+        if (currentRow == correctRow && currentColumn == correctColumn) {
             return true; // Brickan är på rätt plats
         } else {
             return false; // Brickan är på fel plats
