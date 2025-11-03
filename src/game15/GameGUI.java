fa454e79a12a139457abe528a38092e971464cbf
Karwan Ali <karwana0000@gmail.com>
2025-11-03 13:09:45 +0100

    added menu for new game + theme

--- src/game15/GameGUI.java
+++ src/game15/GameGUI.java
@@ -9,20 +9,37 @@ public class GameGUI extends JFrame implements ActionListener {
 
     private GameBoard game;
     private JButton[][] buttons;
+    private JButton newGameButton, themeButton;
 
     public GameGUI() {
         game = new GameBoard();
         game.shuffle();
+
+        setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        newGameButton = new JButton("New Game");
+        themeButton = new JButton("Theme");
+        newGameButton.addActionListener(this);
+        themeButton.addActionListener(this);
+        topPanel.add(newGameButton);
+        topPanel.add(themeButton);
+        add(topPanel, BorderLayout.NORTH);
+
+        JPanel panel = new JPanel(new GridLayout(4,4));
         buttons = new JButton[4][4];
-        setLayout(new GridLayout(4,4));
 
         for (int row = 0; row < 4; row++) {
-            for (int column = 0; column < 4; column++) {
-                buttons[row][column] = new JButton();
-                buttons[row][column].addActionListener(this);
-                add(buttons[row][column]);
+            for (int col = 0; col < 4; col++) {
+                buttons[row][col] = new JButton();
+                buttons[row][col].addActionListener(this);
+                panel.add(buttons[row][col]);
             }
         }
+
+        add(panel, BorderLayout.CENTER);
+
+
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(600, 600);
