5740f25caefaa2d9669e533bc136d5ee4bbce5ce
Karwan Ali <karwana0000@gmail.com>
2025-11-04 13:15:39 +0100

    removed unused setter

--- src/game15/Settings.java
+++ src/game15/Settings.java
@@ -12,32 +12,16 @@ public class Settings {
         return tileColor;
     }
 
-    public void setTileColor(Color tileColor) {
-        this.tileColor = tileColor;
-    }
-
     public Color getEmptyColor() {
         return emptyColor;
-    }
-
-    public void setEmptyColor(Color emptyColor) {
-        this.emptyColor = emptyColor;
     }
 
     public Color getTextColor() {
         return textColor;
     }
 
-    public void setTextColor(Color textColor) {
-        this.textColor = textColor;
-    }
-
     public Color getBackgroundColor() {
         return backgroundColor;
-    }
-
-    public void setBackgroundColor(Color backgroundColor) {
-        this.backgroundColor = backgroundColor;
     }
 
     public void setDarkTheme() {
