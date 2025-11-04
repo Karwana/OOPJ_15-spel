8c6ebda3b6b2e5492184a3e1150bec5f9be9fd52
Fahim Khan <abrar.fahimk98@gmail.com>
2025-11-04 10:40:45 +0100

    added color settings

--- src/game15/Settings.java
+++ src/game15/Settings.java
@@ -1,4 +1,22 @@
 package game15;
 
+import java.awt.*;
+import java.awt.Color;
+
 public class Settings {
+    private Color tileColor;
+    private Color emptyColor;
+    private Color textColor;
+    private Color backgroundColor;
+
+
+    public Settings() {
+        tileColor = new Color(153,102,0);
+        emptyColor = new Color(153,153,153);
+        textColor = new Color(255,204,51);
+        backgroundColor = new Color(153,153,153);
+    }
+
+
+
 }
